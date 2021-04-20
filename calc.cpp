0e90457f310755f66197d9b9dd7e95c249e41751
caleb-cyr <70614456+caleb-cyr@users.noreply.github.com>
2021-04-20 16:12:09 -0400

    Add files via upload

--- calc.cpp
+++ calc.cpp
@@ -6,25 +6,20 @@
 #include <map>
 #include <stdlib.h>
 #include <sstream>
-#include <algorithm>
-#include <pthread.h>
-#include <semaphore.h>
 using std::vector;
 using std::map;
 using std::string;
   struct Calc{
    std::map<std::string,int> vars;
    std::vector<std::string> tokenize(const std::string & expr);
-   pthread_mutex_t lock;
    int calcEval(const char *expr, int * result);
   };
 
 extern "C" struct Calc* calc_create(void){
-   struct Calc * myCalc =  new Calc();
-   pthread_mutex_init(&myCalc->lock,NULL);
-   return myCalc;
+   return new Calc();
 }
 
+//function to divide input into tokens, taken from project description
 std::vector<std::string>Calc:: tokenize(const std::string & expr){
   std::vector<std::string> vec;
   std::stringstream s(expr);
@@ -34,116 +29,118 @@ std::vector<std::string>Calc:: tokenize(const std::string & expr){
   }
   return vec;
 }
+
+//eval function
 int Calc::calcEval(const char *expr, int * result){
   std::vector<std::string> firstVec = tokenize(expr);
   std::vector<std::string> vec;
   std::vector<int> numbVec = {};
   std::vector<char> opVec = {};
   std::string variable;
-  bool myBool = 0;
+
+
+  bool varInitializer = 0;
+
+  //check if variable is being initialized
   if(firstVec.size() > 1 && strcmp(firstVec[1].c_str(),"=") == 0){
-    myBool = 1;
+    varInitializer = 1;
     variable = firstVec[0];
-    for(int i = 0; i < variable.size(); i++){
-      if (!isalpha(variable[i])){
-	return 0;
-      }
-    }
   }
-  if(myBool){
+
+  //if variable is being initialized, push operation statement after equal sign onto new vector
+  //else give vec the operation statement
+  if (varInitializer) {
     for(int i = 2; i < firstVec.size(); i++){
       vec.push_back(firstVec[i]);
     }
-  }
-  else
+  } else {
     vec = firstVec;
+  }
   
-  for(int i = 0; i < vec.size();i++){
+  for (int i = 0; i < vec.size();i++) {
     std::string myTemp = vec[i];
     bool isDig = 1;
-    for(int j = 0; j < myTemp.size(); j++){
-      if(!isdigit(myTemp[j]))
-	isDig = 0;
+    //check if current token is a digit
+    for (int j = 0; j < myTemp.size(); j++) {
+      if (!isdigit(myTemp[j])) {
+	      isDig = 0;
+      }
     }
-    if(isDig){
+    //this section organizes tokens by token type onto different vectors
+    //if it is a digit, push back onto number vector
+    //else if negative number, push back negative onto operator vector and digit onto number vector
+    //else if single char token, check if operator -> push back onto operator vector, else if variable, push back number
+          //that variable represents onto number vector
+    //else: if negative number, push back negative integer representative, else look up variable and push back the number
+          //it represents onto the number vector
+    if (isDig) {
       numbVec.push_back(stoi(vec[i]));
-    }
-    else if(i == 0 && vec[i].size() == 1 && vec[i].front() == 45){
+    } else if (i == 0 && vec[i].size() == 1 && vec[i].front() == 45) {
       numbVec.push_back(0);
       opVec.push_back(vec[i].front());
-    }
-    else if(vec.at(i).size() == 1){
+    } else if(vec.at(i).size() == 1) {
       char myChar = vec[i].front();
-      if(myChar == 47 || myChar == 45 || myChar == 42 || myChar == 43)
-	opVec.push_back(myChar);
-      else if(vars.find(vec[i]) != vars.end()){
-	numbVec.push_back(vars.find(vec.at(i))->second);
-	}
-    }
-    else{
-      if(vec.at(i).front() == 45){
-	numbVec.push_back(stoi(vec.at(i)));
-      }
-      else{
-      std::string checker = vec[i];
-      if(vars.find(checker) != vars.end()){
-	numbVec.push_back(vars.find(checker)->second);
-      }
-      else
-	return 0;
+      if (myChar == 47 || myChar == 45 || myChar == 42 || myChar == 43) {
+	      opVec.push_back(myChar);
+      } else if (vars.find(vec[i]) != vars.end()) {
+	      numbVec.push_back(vars.find(vec.at(i))->second);
+	    }
+    } else {
+      if (vec.at(i).front() == 45) {
+	      numbVec.push_back(stoi(vec.at(i)));
+      } else {
+        std::string checker = vec[i];
+        if (vars.find(checker) != vars.end()) {
+	        numbVec.push_back(vars.find(checker)->second);
+        }
       }
     }
   }
       
+  //invalid expression check
   if(numbVec.size() != opVec.size()+1){
     return 0;
   }
-  std::reverse(numbVec.begin(),numbVec.end());
-  std::reverse(opVec.begin(),opVec.end()); 
-  while(numbVec.size() > 1 ){
-    if(opVec.back() == 43){
+  while(numbVec.size() > 1) {
+    if(opVec.back() == 43) {    //addition operation
       int one = numbVec.back();numbVec.pop_back();
       int two = numbVec.back();numbVec.pop_back();
       int myResult = one + two;
       numbVec.push_back(myResult);
       opVec.pop_back();
-    }
-    else if(opVec.back() == 45){
+    } else if(opVec.back() == 45) {   //subtraction operation
       int one = numbVec.back();numbVec.pop_back();
       int two = numbVec.back();numbVec.pop_back();
-      int myResult = one - two;
+      int myResult = two - one;
       numbVec.push_back(myResult);
       opVec.pop_back();
-    }
-    else if (opVec.back() == 42){
+    } else if (opVec.back() == 42) {    //multiplication operation
       int one = numbVec.back();numbVec.pop_back();
       int two = numbVec.back();numbVec.pop_back();
-      int myResult = one*two;
+      int myResult = one * two;
       numbVec.push_back(myResult);
       opVec.pop_back();
-    }
-    else{
+    } else {                            //division operation
       int one = numbVec.back();numbVec.pop_back();
       int two = numbVec.back();numbVec.pop_back();
-      if(two == 0)
-	return 0; 
-      int myResult = one/two;
+      if(one == 0) {
+	      return 0; 
+      }
+      int myResult = two/one;
       numbVec.push_back(myResult);
       opVec.pop_back();
     }
   }
   
   *result = numbVec.back();
-  if(myBool){
-    pthread_mutex_lock(&lock);
+  //initialize variable if not in the map already
+  if(varInitializer){
     vars.insert(std::pair<std::string,int>(variable,numbVec.back()));
-    pthread_mutex_unlock(&lock);
   }
   return numbVec.back();
 }
   
 extern "C" void calc_destroy(struct Calc *calc){
-  pthread_mutex_destroy(&calc->lock);
   delete(calc);  
 }
 
--- calcServer.c
+++ calcServer.c
@@ -1,78 +1,54 @@
 #include <stdio.h>      /* for snprintf */
 #include "csapp.h"
 #include "calc.h"
-#include <pthread.h>
-#include "thread.h"
 
-void *worker(void * args){
-  struct connInfo *myInfo = args;
-  pthread_detach(pthread_self());
-  int keep_going = 1;
-  while(keep_going){
-  keep_going = chat_with_client(myInfo->myCalc,myInfo->clientfd);
-  }
-  close(myInfo->clientfd);
-  free(myInfo);
-  return NULL;
-}
+
 int chat_with_client(struct Calc * myCalc,int client){
-	
-   rio_t rio;
+  rio_t rio;
   int result = 0;
-  rio_readinitb(&rio,client); 
+  rio_readinitb(&rio,client);   //set up empty read buffer
   char line[RIO_BUFSIZE];
   int done = 0;
-  while(!done){
-    ssize_t n = rio_readlineb(&rio,line,RIO_BUFSIZE);
-    line[n] = '\0';
-    if(n <= 0)
+  while(!done) {   //while chatting
+    ssize_t n = rio_readlineb(&rio,line,RIO_BUFSIZE);   //read line
+    line[n] = '\0';   //null terminate line
+    if (n <= 0) { //check if nothing is read, and return from function if so
       done = 1;
-    else if(strcmp(line,"quit\n") == 0 || strcmp(line,"quit\r\n") == 0){
+    } else if (strcmp(line,"quit\n") == 0 || strcmp(line,"quit\r\n") == 0) {    //return from function if quit command is read
      done = 1;
-     return 1;
-    }
-    else if(strcmp(line,"shutdown\n") == 0 || strcmp(line,"shutdown\r\n") == 0){ 
+     return 1;    //return 1 to signal session ending
+    } else if (strcmp(line,"shutdown\n") == 0 || strcmp(line,"shutdown\r\n") == 0){    //return from function if shutdown command is read
       done = 1;
-      return 0;
-    }
-    else{
-      if(calc_eval(myCalc,line,&result) == 0)
-	rio_writen(client,"ERROR\n",6);
-      else{
-	int len = snprintf(line,RIO_BUFSIZE,"%d\n",result);
-        if(len < RIO_BUFSIZE){
-	  rio_writen(client,line,len);
-	}
+      return 0;   //return 0 to signal server process exit
+    } else {
+      if (calc_eval(myCalc,line,&result) == 0) {    //catch error if thrown from invalid expression
+	      rio_writen(client,"ERROR\n",6);
+      } else {  //else print result from calc eval function to next line
+	      int len = snprintf(line,RIO_BUFSIZE,"%d\n",result);   
+        if(len < RIO_BUFSIZE) {
+	        rio_writen(client,line,len);
+	      }
       }
     }
   }
-  return 1;
 }
 
 int main(int argc, char **argv) {
-	/* TODO: implement this program */
-   if (argc < 2)
-    return 1;
-   char * port = argv[1];
-   char * webroot = argv[2];
+   char * port = argv[1];               //get port number argument from command line
+   int server = open_listenfd(port);    //open server port
+   int keep_going = 1;
    struct Calc * myCalc = calc_create();
-   int client;
-   int server = open_listenfd(port);
-   while(1){
-     client = Accept(server,NULL,NULL);
-     if(client > 0){
-     struct connInfo * info = malloc(sizeof(struct connInfo));
-     info->clientfd = client;
-     info->webroot = webroot;
-     info->myCalc = myCalc;
-     pthread_t newThread;
-     if(pthread_create(&newThread,NULL,worker,info) != 0)
-       return 0;
+   //while client is connected, keep port open
+   //close when client disconnects
+   while (keep_going) {     //keep_going will be set to zero on shutdown command, set to one on quit command
+     int client = Accept(server,NULL,NULL);             //accept user input from server port
+     if(client > 0) {
+       keep_going = chat_with_client(myCalc,client);    //set while loop indicator based on return value from chat_with_client function
+       Close(client);                                   //close current client connection
      }
    }
    return 0;
 }
-
 
     
   
